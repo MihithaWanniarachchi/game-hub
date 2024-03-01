5fdc58f7004cb1e7e46267614d36b35aadb0b548
MihithaWanniarachchi <wanniarachchimihitha@gmail.com>
2024-03-01 17:08:32 +0530

    Refactor gameGrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,15 +15,16 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
+  if (error) return <Text>{error}</Text>;
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      {/*Number of cards to display according to the screen size */}
+   
+     
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
         spacing={6}
       >
+         {/*Number of cards to display according to the screen size */}
         {isLoading &&
           skeletons.map((Skeleton) => (
             <GameCardContainer key={Skeleton}>
@@ -37,7 +38,6 @@ const GameGrid = ({ gameQuery }: Props) => {
           </GameCardContainer>
         ))}
       </SimpleGrid>
-    </>
   );
 };
 
